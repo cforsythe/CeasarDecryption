a5455828b9f70d78ff4adbcdd24ef763540d11d8
Chris <cforsythe@csumb.edu>
2017-05-01 01:06:21 -0700

    Added some code to read common words into hashmap, decyrptText is WIP

--- CeasarDecryption.java
+++ CeasarDecryption.java
@@ -1,4 +1,15 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -8,9 +19,34 @@ import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class CeasarDecryption extends Application{
-
-	int key = 0;
 	
+	Map<String, Integer> commonWords = new HashMap<String, Integer>();
+	int key = 0;
+	public void fillMap(Map<String, Integer> commonWords)
+	{
+	URL path = CeasarDecryption.class.getResource("mostCommonWords.txt");
+	File f = new File(path.getFile());
+	try (BufferedReader br = new BufferedReader(new FileReader(f))) {
+		String line;
+		while((line = br.readLine()) != null)
+		{
+			commonWords.put(line, 0);
+		}
+	}
+	catch(FileNotFoundException e)
+	{
+		System.out.println("File not found");
+	}
+	catch(IOException e)
+	{
+		System.out.println("Error reading file");
+	}
+	}
+	public String decryptText(String encrypted)
+	{
+	System.out.println("Nothing");
+	return "";
+	}
 	public static void main(String[] args)
 	{
 	launch(args);
@@ -46,5 +82,9 @@ public class CeasarDecryption extends Application{
 		rootNode.getChildren().addAll(inputBox, decrypt, decTitle, decKey, decText);
 		myStage.setScene(myScene);
 		myStage.show();
+		
+		fillMap(commonWords);
+		decrypt.setOnAction(e -> decText.setText(decryptText(decrypt.getText())));
+		
 	}
 }
